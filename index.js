dbcc0e575dcf4bab55b64f9462868e6d8a86cb39
Marcos Vinícius Correa Rodrigues <rodrigues.mvinicius@gmail.com>
2022-11-17 10:42:23 -0300

    FINAL PUSH

--- index.js
+++ index.js
@@ -1,99 +1,115 @@
-const { ethers } = require("ethers");
-const { abi } = require("./contracts/FundMe.json");
-const { FUNDME_CONTRACT } = require("./constants");
-const { Web3 } = require("web3");
+import { ethers } from "./ethers-5.6.esm.min.js"
+import { abi, contractAddress } from "./constants.js"
+
+const connectButton = document.getElementById("connectButton")
+const withdrawButton = document.getElementById("withdrawButton")
+const fundButton = document.getElementById("fundButton")
+const balanceButton = document.getElementById("balanceButton")
+
+const treasurelabel = document.getElementById("treasurelabel")
+
+connectButton.onclick = connect
+withdrawButton.onclick = withdraw
+fundButton.onclick = fund
+balanceButton.onclick = getBalance
+
+window.ethereum.on("accountsChanged", (accounts) => {
+    if (accounts.length > 0) {
+        connectButton.innerHTML = "Connected:" + accounts[0]
+    }
+})
+
+window.ethereum.on("chainChanged", (chainId) => {
+    if (chainId != 5 && chainId != 31337) {
+        connectButton.innerHTML = "WRONG NETWORK! CHANGE TO GOERLI OR LOCALHOST"
+    } else {
+        connectButton.innerHTML = "Connect"
+    }
+})
 
 async function connect() {
-  if (typeof window.ethereum !== "undefined") {
-    try {
-      await ethereum.request({ method: "eth_requestAccounts" });
-    } catch (error) {
-      console.log(error);
+    if (typeof window.ethereum !== "undefined") {
+        try {
+            await ethereum.request({ method: "eth_requestAccounts" })
+        } catch (error) {
+            console.log(error)
+        }
+
+        const accounts = await ethereum.request({ method: "eth_accounts" })
+        if (window.ethereum.isConnected()) {
+            connectButton.innerHTML = "Connected:" + accounts
+            getBalance()
+        }
+    } else {
+        connectButton.innerHTML = "Please install MetaMask"
     }
-
-    const accounts = await ethereum.request({ method: "eth_accounts" });
-
-    if (window.ethereum.isConnected())
-      document.getElementById("connectButton").innerHTML =
-        "Connected:" + accounts;
-  } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please install MetaMask";
-  }
 }
 
 async function withdraw() {
-  console.log(`Withdrawing...`);
-  if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const contract = new ethers.Contract(FUNDME_CONTRACT, abi, signer);
-    try {
-      const transactionResponse = await contract.withdraw();
-      await listenForTransactionMine(transactionResponse, provider);
-    } catch (error) {
-      console.log(error);
+    console.log(`Withdrawing...`)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        await provider.send("eth_requestAccounts", [])
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.withdraw()
+            await listenForTransactionMine(transactionResponse, provider)
+            // await transactionResponse.wait(1)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        withdrawButton.innerHTML = "Please install MetaMask"
     }
-  } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please install MetaMask";
-  }
 }
 
-async function fund(ethAmount) {
-  console.log(`Funding with ${ethAmount}...`);
-  if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    const signer = provider.getSigner();
-    const contract = new ethers.Contract(FUNDME_CONTRACT, abi, signer);
-    try {
-      const transactionResponse = await contract.fund({
-        value: ethers.utils.parseEther(ethAmount),
-      });
-      await listenForTransactionMine(transactionResponse, provider);
-    } catch (error) {
-      console.log(error);
+async function fund() {
+    const ethAmount = document.getElementById("ethAmount").value
+    console.log(`Funding with ${ethAmount}...`)
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        const signer = provider.getSigner()
+        const contract = new ethers.Contract(contractAddress, abi, signer)
+        try {
+            const transactionResponse = await contract.fund({
+                value: ethers.utils.parseEther(ethAmount),
+            })
+            await listenForTransactionMine(transactionResponse, provider)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        fundButton.innerHTML = "Please install MetaMask"
     }
-  } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please install MetaMask";
-  }
 }
 
 async function getBalance() {
-  if (typeof window.ethereum !== "undefined") {
-    const provider = new ethers.providers.Web3Provider(window.ethereum);
-    try {
-      console.log(FUNDME_CONTRACT);
-      const balance = await provider.getBalance(FUNDME_CONTRACT);
-      console.log(ethers.utils.formatEther(balance));
-      document.getElementById("treasurelabel").innerHTML =
-        "Current Treasure: " + ethers.utils.formatEther(balance);
-    } catch (error) {
-      console.log(error);
+    if (typeof window.ethereum !== "undefined") {
+        const provider = new ethers.providers.Web3Provider(window.ethereum)
+        try {
+            const balance = await provider.getBalance(contractAddress)
+            console.log(ethers.utils.formatEther(balance))
+            treasurelabel.innerHTML = "Treasure Funds: " + ethers.utils.formatEther(balance)
+        } catch (error) {
+            console.log(error)
+        }
+    } else {
+        balanceButton.innerHTML = "Please install MetaMask"
     }
-  } else {
-    document.getElementById("connectButton").innerHTML =
-      "Please install MetaMask";
-  }
 }
 
 function listenForTransactionMine(transactionResponse, provider) {
-  console.log(`Mining ${transactionResponse.hash}`);
-  return new Promise((resolve, reject) => {
-    provider.once(transactionResponse.hash, (transactionReceipt) => {
-      console.log(
-        `Completed with ${transactionReceipt.confirmations} confirmations. `
-      );
-      getBalance();
-      resolve();
-    });
-  });
+    console.log(`Mining ${transactionResponse.hash}`)
+    return new Promise((resolve, reject) => {
+        try {
+            provider.once(transactionResponse.hash, (transactionReceipt) => {
+                console.log(`Completed with ${transactionReceipt.confirmations} confirmations. `)
+                getBalance()
+                resolve()
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
 }
-
-module.exports = {
-  connect,
-  withdraw,
-  fund,
-  getBalance,
-};
